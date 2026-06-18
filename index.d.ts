ac1a2cb139e225b53083e25d3ae01873206463ee
THANOOJA S <thanu28102006@gmail.com>
2026-06-18 13:01:43 +0530

    Add files via upload

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,5 @@
-export * from "./products";
-export * from "./bills";
+export * from "./generated/api";
+export * from "./generated/api.schemas";
+export { setBaseUrl, setAuthTokenGetter } from "./custom-fetch";
+export type { AuthTokenGetter } from "./custom-fetch";
 //# sourceMappingURL=index.d.ts.map
